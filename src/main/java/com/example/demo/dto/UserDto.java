60ef5318eae4777ce46eb2c46cad40fdf7728bef
aya-mrah <aya.mrah@etudiant-isi.utm.tn>
2021-12-12 19:45:03 +0100

    u

--- src/main/java/com/example/demo/dto/UserDto.java
+++ src/main/java/com/example/demo/dto/UserDto.java
@@ -17,24 +17,5 @@ public class UserDto {
     private String username;
     @Email
     private String email;
-    /*
-    public long getId() {
-        return id;
-    }
-    public void setId(long id) {
-       this.id = id;
-    }
-    public String getUsername() {
-        return username;
-    }
-    public void setUsername(String username ) {
-        this.username = username;
-    }
-    public String getEmail() {
-        return email;
-    }
-    public void setEmail(String email) {
-        this.email = email;
-    }
-*/
+
 }
